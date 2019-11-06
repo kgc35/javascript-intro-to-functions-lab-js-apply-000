a356c4a354706280824eb7430aa4019496080a24
Kent Chan <kntchn@gmail.com>
2019-11-06 22:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } //checks if the string is lowercase
 
+  else if string.toUpperCase() === string {
+    return "YES INDEED!"
+  } //checks if string is upper case.
+
+  else {
+    return "I love you, too"
+  }
+}
 
 //  string.toUpperCase() === string //checks if the string is uppercase
 //}
