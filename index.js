96824b983eb0a5aa7e975cee96fd3bd54e30ea72
Kent Chan <kntchn@gmail.com>
2019-11-06 22:06:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper(string))
 }
+
+function sayHiToGrandma(string) {
+  if string.toLowerCase() === string {
+    return "I can't hear you!"
+  } //checks if the string is lowercase
+
+
+  string.toUpperCase() === string //checks if the string is uppercase
+}
