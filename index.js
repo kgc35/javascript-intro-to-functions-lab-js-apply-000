298771d33b13be01e7249212bd8a86487845f3fa
Kent Chan <kntchn@gmail.com>
2019-11-06 22:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 
-  if whisper(string) === string {
+  if (whisper(string) === string) {
     return "I can't hear you!"
   } //checks if the string is lowercase
 
-  else if shout(string) === string {
+  else if (shout(string) === string) {
     return "YES INDEED!"
   } //checks if string is upper case.
 
